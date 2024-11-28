aa584fe07bad11ed165c3c56f3611383661848e5
Nikolaj <nikolaj.tetradov@gmail.com>
2024-11-28 14:02:59 +0200

    product showcase animations

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,6 +1,19 @@
+'use client';
+
 import Image from 'next/image';
 import appScreen from '../assets/images/app-screen.png';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useEffect, useRef } from 'react';
 export const ProductShowcase = () => {
+  const appImage = useRef<HTMLImageElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: appImage,
+    offset: ['start end', 'end end'],
+  });
+
+  const rotateX = useTransform(scrollYProgress, [0, 1], [15, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+
   return (
     <div className='bg-black text-white bg-gradient-to-b from-black to-[#5D2CA8] py-[72px] sm:py-24'>
       <div className='container py-24 sm:py-32'>
@@ -15,13 +28,22 @@ export const ProductShowcase = () => {
             make a lasting impression.
           </p>
         </div>
-        <Image
-          src={appScreen}
-          alt='App Screenshot'
-          // width={1000}
-          // height={500}
-          className='mx-auto mt-14 mb-20'
-        />
+        <motion.div
+          style={{
+            opacity: opacity,
+            rotateX: rotateX,
+            transformPerspective: '800px',
+          }}
+        >
+          <Image
+            src={appScreen}
+            alt='App Screenshot'
+            // width={1000}
+            // height={500}
+            className='mx-auto mt-14 mb-20'
+            ref={appImage}
+          />
+        </motion.div>
       </div>
     </div>
   );
